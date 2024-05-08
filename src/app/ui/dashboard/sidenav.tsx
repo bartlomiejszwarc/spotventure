a20f558e44937d1f4f88e5644fc55ad25f222a6d
bartlomiejszwarc <szwarc.it@gmail.com>
2024-05-08 22:46:27 +0200

    Adjusted sidenav design and slide animation

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,36 +1,59 @@
 'use client';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './styles.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import useWindowDimensions from './../../../hooks/useWindowDimensions';
 
 function SideNav() {
-  const [style, setStyle] = useState<string>('closed-sidebar-initial');
+  const [isMobile, setIsMobile] = useState<boolean>(true);
   const [open, setOpen] = useState<boolean>(false);
+  const [style, setStyle] = useState<string>('');
+  const {width} = useWindowDimensions();
+
+  useEffect(() => {
+    if (width && width > 640) {
+      setIsMobile(false);
+      setStyle('closed-sidenav-initial');
+      setOpen(false);
+    } else {
+      setIsMobile(true);
+      setOpen(false);
+      setStyle('closed-sidenav-initial-mobile');
+    }
+  }, [width]);
+
+  useEffect(() => {
+    if (!open && !isMobile) {
+      setStyle('closed-sidenav');
+    }
+  }, [open]);
 
   const changeStyle = () => {
     if (open) {
-      setStyle('closed-sidebar slide-left');
+      setStyle('closed-sidenav');
     } else {
-      setStyle('opened-sidebar');
+      setStyle('opened-sidenav');
     }
   };
+
   return (
     <div>
-      {open}
       {!open ? (
         <MenuIcon
-          className='absolute left-5 top-5 text-slate-800 text-4xl cursor-pointer'
+          className={`absolute left-5 top-5  ${
+            !isMobile ? 'text-slate-300' : 'text-slate-800'
+          }   text-4xl cursor-pointer`}
           onClick={() => {
             changeStyle();
-            setOpen(!open);
+            setOpen(true);
           }}></MenuIcon>
       ) : (
         <CloseIcon
-          className='absolute left-5 top-5 text-slate-300 text-4xl cursor-pointer'
+          className={`absolute left-5 top-5 text-slate-300 text-4xl cursor-pointer`}
           onClick={() => {
             changeStyle();
-            setOpen(!open);
+            setOpen(false);
           }}></CloseIcon>
       )}
       {open ? <div className={style}></div> : <div className={style}></div>}
