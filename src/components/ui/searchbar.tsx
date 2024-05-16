8adcf8be6ecc6432a2a8b02e8273aca241b69838
bartlomiejszwarc <szwarc.it@gmail.com>
2024-05-16 12:34:44 +0200

    feat: animated search bar

--- src/components/ui/searchbar.tsx
+++ src/components/ui/searchbar.tsx
@@ -1,7 +1,27 @@
+'use client';
+import './styles.css';
+
+import React, {useState} from 'react';
 import SearchIcon from '@mui/icons-material/Search';
+
 function SearchBar() {
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
+  const handleOutOfFocus = () => {
+    setFocused(false);
+  };
+
   return (
-    <div className='w-full h-12 rounded-full border-2 border-zinc-300 flex items-center justify-between pr-4 pl-6'>
+    <div
+      className={`w-full lg:w-96 h-12 rounded-full border-2 bg-zinc-100 border-zinc-300 flex items-center justify-between pr-4 pl-6 ${
+        focused ? 'focused-full-width' : 'unfocused'
+      }`}
+      onClick={handleFocus}
+      onMouseOut={handleOutOfFocus}>
       <input
         type='text'
         className='w-full bg-transparent h-[70%] placeholder:text-lg outline-none'
@@ -11,4 +31,5 @@ function SearchBar() {
     </div>
   );
 }
+
 export default SearchBar;
