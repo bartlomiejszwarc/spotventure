d58b047cff4fd1ac2fabde171b03c81693302921
bartlomiejszwarc <szwarc.it@gmail.com>
2024-06-06 18:05:52 +0200

    feat: routing to post page

--- src/components/ui/card/post/postPreviewCard.tsx
+++ src/components/ui/card/post/postPreviewCard.tsx
@@ -7,6 +7,8 @@ import UserAvatar from '../../user-avatar';
 import PostLikes from './postLikes';
 import UsernameLink from './usernameLink';
 import convertDate from '@/utils/convertDate';
+import Link from 'next/link';
+import {useRouter} from 'next/navigation';
 
 interface IPostPreview {
   id: string | undefined;
@@ -31,7 +33,7 @@ function PostPreviewCard({
 }: IPostPreview) {
   const [name, setName] = useState<string>('');
   const [userProfileImageUrl, setUserProfileImageUrl] = useState<string | null>();
-
+  const router = useRouter();
   const {getUserData} = useUserData();
 
   useEffect(() => {
@@ -46,35 +48,42 @@ function PostPreviewCard({
 
   if (name) {
     return (
-      <div className=' h-[12rem] lg:h-[17rem] border-0 rounded-xl border-zinc-400 relative bg-zinc-100 shadow-md shadow-zinc-400 '>
-        <div className='h-3/5 lg:h-[70%] relative'>
-          <div className='absolute inset-0'>
-            <Image className='rounded-t-xl' layout='fill' objectFit='cover' src={imageUrl} alt='Place' />
-          </div>
-          <div className='absolute flex w-full h-full items-end justify-between px-3 pb-1 opacity-80'>
-            <div className='flex space-x-1 bg-zinc-600/80 text-zinc-300 pr-[7px] pl-2 rounded-full bg-opacity-60 py-[2px]'>
-              <span className='truncate text-xxs md:text-xs xl:text-sm '>
-                <time dateTime={visitDate!?.toString()}>{convertDate(visitDate, 'LLLL d, yyyy')}</time>
-              </span>
-              <AccessTimeIcon className='text-sm lg:text-lg ' />
+      <div className='relative h-[12rem] lg:h-[17rem]'>
+        <div
+          className=' h-[12rem] lg:h-[17rem] rounded-xl relative bg-zinc-100 shadow-lg shadow-zinc-300 '
+          onClick={() => router.push(`/spot/${id}`)}>
+          <div className='h-3/5 lg:h-[70%] relative'>
+            <div className='absolute inset-0'>
+              <Image className='rounded-t-xl' layout='fill' objectFit='cover' src={imageUrl} alt='Place' />
+            </div>
+            <div className='absolute flex w-full h-full items-end justify-between px-3 pb-1 opacity-80'>
+              <div className='flex space-x-1 bg-zinc-600/80 text-zinc-300 pr-[7px] pl-2 rounded-full bg-opacity-60 py-[2px]'>
+                <span className='truncate text-xxs md:text-xs xl:text-sm '>
+                  <time dateTime={visitDate!?.toString()}>{convertDate(visitDate, 'LLLL d, yyyy')}</time>
+                </span>
+                <AccessTimeIcon className='text-sm lg:text-lg ' />
+              </div>
+            </div>
+            <div className='absolute flex w-full h-full items-start justify-end pt-1 px-3 opacity-90 '>
+              <div className='flex space-x-[2px] items-center justify-start pl-[5px] pr-2 text-zinc-300 bg-zinc-600/80 rounded-full max-w-36 backdrop-blur-lg py-[2px]'>
+                <PlaceIcon className='text-sm lg:text-lg ' />
+                <span className='truncate text-xxs md:text-xs xl:text-sm text-zinc-300'>{location}</span>
+              </div>
             </div>
           </div>
-          <div className='absolute flex w-full h-full items-start justify-end pt-1 px-3 opacity-90 '>
-            <div className='flex space-x-[2px] items-center justify-start pl-[5px] pr-2 text-zinc-300 bg-zinc-600/80 rounded-full max-w-36 backdrop-blur-lg py-[2px]'>
-              <PlaceIcon className='text-sm lg:text-lg ' />
-              <span className='truncate text-xxs md:text-xs xl:text-sm text-zinc-300'>{location}</span>
-            </div>
-          </div>
-          <UserAvatar
-            profileImageUrl={userProfileImageUrl as string}
-            name={name}
-            className='lg:h-10 lg:w-10 border-0 absolute right-1/2 translate-x-1/2 bottom-0 translate-y-[50%]'
-          />
         </div>
-
-        <div className='flex flex-col w-full items-center pt-6 overflow-hidden'>
-          <UsernameLink uid={uid} name={name} />
-          <PostLikes id={id as string} likedByIds={likedByIds} uid={uid} />
+        <div className='w-full absolute top-0 translate-y-[7.2rem] lg:translate-y-[11.9rem] z-30 flex flex-col items-center bg-red-500'>
+          <div className='flex flex-col absolute translate-y-[-50%] items-center z-30'>
+            <UserAvatar
+              profileImageUrl={userProfileImageUrl ? (userProfileImageUrl as string) : profileImageUrl}
+              name={name}
+              className='w-12 h-12'
+            />
+          </div>
+          <div className='flex flex-col items-center pt-6 overflow-hidden absolute z-30'>
+            <UsernameLink uid={uid} name={name} />
+            <PostLikes id={id as string} likedByIds={likedByIds} uid={uid} />
+          </div>
         </div>
       </div>
     );
