d0c120c50ca34259a16279fcb5c685cc26bf75d3
bartlomiejszwarc <szwarc.it@gmail.com>
2024-07-03 11:13:30 +0200

    fix: renamed background svg properties

--- src/components/ui/background/background.tsx
+++ src/components/ui/background/background.tsx
@@ -18,15 +18,15 @@ export default function Background() {
       <rect fill='#ffffff' width='24' height='24' />
       <defs>
         <linearGradient id='a' x1='0' x2='0' y1='0' y2='1' gradientTransform='rotate(118,0.5,0.5)'>
-          <stop offset='0' stop-color='#34D399' />
-          <stop offset='1' stop-color='#E879F9' />
+          <stop offset='0' stopColor='#34D399' />
+          <stop offset='1' stopColor='#E879F9' />
         </linearGradient>
       </defs>
       <pattern id='b' width='29' height='29' patternUnits='userSpaceOnUse'>
         <circle fill='#ffffff' cx='14.5' cy='14.5' r='14.5' />
       </pattern>
       <rect width='100%' height='100%' fill='url(#a)' />
-      <rect width='100%' height='100%' fill='url(#b)' fill-opacity='0.05' />
+      <rect width='100%' height='100%' fill='url(#b)' fillOpacity='0.05' />
     </svg>
   );
 }
